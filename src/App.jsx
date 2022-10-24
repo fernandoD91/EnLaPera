f879b9a3e427988b12672420089c207b9a8654e3
Ingrid Lima <ingridlimas@hotmail.com>
2022-10-23 22:36:34 -0300

    new-update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import { Home } from './Pages'
 import Cards from './componentes/organisms/Cards'
+import Input from './componentes/atoms/Inputs'
 
 function App() {
 
@@ -8,6 +9,9 @@ function App() {
     <div className="App">
       <Home/>
       <Cards/>
+      <label htmlFor="name">Ingrese nombres: </label>
+      <Input mode={'pass'} impChange={(e)=> console.log(e.target.value)}/>
+      
     </div>
   )
 }
