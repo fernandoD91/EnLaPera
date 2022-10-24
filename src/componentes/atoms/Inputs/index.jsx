ac02c0a7c65e438a740ef339d64694a7f2abef78
Ingrid Lima <ingridlimas@hotmail.com>
2022-10-23 22:18:02 -0300

    input-update

---
+++ src/componentes/atoms/Inputs/index.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+// import moduleStyle from './style.module.css';
+import moduleStyle from './style.module.css';
+
+const Input = ({impChange, name, mode}) => {
+
+    return (  
+        <>
+            {/* <label htmlFor="name">Nombre: </label> */}
+
+            <input 
+                className={moduleStyle[mode]}
+                type="text" 
+                id="name" 
+                name="name" 
+                value={name} 
+                onChange={impChange}
+            />
+        </>
+    );
+}
+ 
+export default Input;
