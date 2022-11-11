542c8349a4e5a7b59ec84a34f0b0a957314f235a
Ingrid Lima <ingridlimas@hotmail.com>
2022-11-11 20:16:40 -0300

    drinkCounter test

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,37 @@ import './App.css'
 import { Home } from './Pages'
 import Cards from './componentes/organisms/Cards'
 import Input from './componentes/atoms/Inputs'
+import AddDrink from './componentes/organisms/DrinksCounter.jsx'
+// import DrinksCounter from './componentes/organisms/DrinksCounter.jsx'
 
 function App() {
 
+  const lista = [
+    {name: 'Juana', count: 4},
+    {name: 'Maria', count: 2},
+    {name: 'Marcos', count: 1}
+  ]
+
+  // const lista = [
+  //   {name: 'Juana'},
+  //   {name: 'Maria'},
+  //   {name: 'Marcos'}
+  // ]
+
+
   return (
     <div className="App">
-      <Home/>
-      <Cards/>
+      {/* <DrinksCounter lista={lista}/> */}
+      
+      {/* <Home/> */}
+
+      {/* <AddDrink listado={lista} /> */}
+
+      <AddDrink listado={lista} id={1} nro={10}/>
+
+      {/* <Cards/>
       <label htmlFor="name">Ingrese nombres: </label>
-      <Input mode={'pass'} impChange={(e)=> console.log(e.target.value)}/>
+      <Input mode={'pass'} impChange={(e)=> console.log(e.target.value)}/> */}
       
     </div>
   )
--- src/componentes/organisms/DrinksCounter.jsx/index.jsx
+++ src/componentes/organisms/DrinksCounter.jsx/index.jsx
@@ -1,6 +1,57 @@
+import { useState } from "react";
+import { useEffect } from "react";
+
+const AddDrink = ({listado, id, nro}) => {
+
+    // Se puede usar el useContext?
+    const [list, setList] = useState(listado)
+
+    const sumatoria = () => {
+        console.log(list)
+        console.log("Tragos que tiene: "+list[id].count);
+        console.log("Toma: "+ nro +" tragos");
+
+        // const num= ()=>{
+        //     return list[id].count + nro;
+        //     // list[]
+        // }
+        // num();
+        // setList({...list, count: num})
+        
+        // console.log(list[0].count);
+        // list.map(usuario => {
+        //     (usuario.id===id) ? usuario.count + nro : console.log("No se pudo hacer la suma");
+        // })
+        list.map(usuario => {
+            typeof list === 'object' ? usuario.count + nro : console.log("No se pudo hacer la suma");
+        })
+        // console.log(list[id].count+nro)
+        setList({...list[id].count+nro, nro})
+    }
+    // 
+    useEffect(() => {
+      sumatoria();
+      
+    }, [])
+    
+    return (
+        <div>
+             <ul>
+                {
+                    listado.map((user, index)=> (<li key={index}>{user.name}: {user.count}</li>))
+                }
+             </ul>
+            
+        </div>
+    );
+
+    }
+export default AddDrink;
 
 
-const DrinksCounter = ({lista}) => {
+
+// Componente que imprime
+// const DrinksCounter = ({lista}) => 
     
     // const lista = [
     //     {name: 'Juana', count: 4},
@@ -8,20 +59,16 @@ const DrinksCounter = ({lista}) => {
     //     {name: 'Marcos', count: 1}
     // ]
     
-    const addDrinks = () => {
-        
-    }
 
-    return (
-        <div>
-            <ul>
-                {
-                    lista.map((user, index)=> <li key={index}>{user.name}: {user.count}</li>)
-                }
-            </ul>
-            
-        </div>
-    );
-}
+    // {lista, nroTrago}
+    // Recibe lista de nombres
+    // Cantidad de tragos (json)
+    
+    
+
+//     return (
+//         
+//     );
+// }
  
-export default DrinksCounter;
+// export default DrinksCounter;
